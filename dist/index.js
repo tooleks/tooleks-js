23b8238fa6c2617d860b3e43b0272867d89d53f0
tooleks <tooleks@gmail.com>
2018-07-18 22:15:17 +0300

    Removed buggy behavior

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-/*! tooleks v1.4.3. Copyright (c) Oleksandr Tolochko. */
+/*! tooleks v1.4.4. Copyright (c) Oleksandr Tolochko. */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
